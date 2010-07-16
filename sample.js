6120bcc87a24ac74dab4de901dba2eb4b4759e2c
patrick.cloke <devnull@localhost>
2010-07-16 10:05:11 -0400

    First pass API and a little code

--- sample.js
+++ sample.js
@@ -3,49 +3,69 @@ function Lobot(username, modules) {
 	this.username = username;
 };
 Lobot.prototype = {
-	onMessageReceive: function(user, time, message) {
+	addModules: function(modulePacks) {
+		// Load each module into the list
+		for each (modulePack in modulePacks) {
+			var metRequirements = true;
+			if (modulePack.requires)
+				metRequirements = false;
+				for (requirement in modulePack.requires)
+					if (!(requirement in this.modules))
+						break;
+			if (metRequirements)
+				this.modules = this.modules.concat(modulePack);
+		}
+		
+		// Check requirements and run start up
+		/*var requirementsMet = false;
+		while (!requirementsMet)
+			for (var i = 0; i < this.modules.length; i++) {
+				var module = this.modules[i];
+				if (module.requires)
+					for (requirement in module.requires)
+						if(!(requirement in this.modules)) {
+							this.modules = this.modules.splice(i,1); // Remove this element
+							break;
+						}
+			}
+		for each (module in this.modules)
+			module.startup();*/
+		alert(this.modules.length);
+	},
+	told: function(user, time, message) {
 		message = message.toLowerCase();
-		let words = message.split(/\b/);
+		var words = message.split(/\b/);
 		
 		for each (module in modules) {
 			if (words[0] in module.verbs) {
-				let response = module.onMessage();
+				var response = module.onMessage();
 
 				return true;
 			}
 		}
-	},
-	loadModules: function(modulePacks) {
-		for each (modulePack in modulePacks)
-			for each (module in modulePack) {
-				this.modules.push(module);
-				this.modules[modules.length - 1].onStartUp();
-			}
 	}
 }
 
 
 // Initiate with a constructor
-let bot = new Lobot(/* Optionally an array of modules */);
+var bot = new Lobot(/* Optionally an array of modules */);
 
-lobot.addModule(helloWorld);
+bot.addModules([helloWorld]);
 
-let helloWorld = {
-	[{
-		onStartUp: function() {},
-		verbs: ["hi", "hello"],
-		requiresAuth: false, // Requires the user to be authenticated with the bot
-		requiresDirect: false, // Requires the message to refer to the bot
-		onMessage: function() {
-			return {responseText: ["", ""], 
+var helloWorld = {
+	[
+		{
+			startup: function() {},
+			verbs: ["hi", "hello"],
+			requiresAuth: false, // Requires the user to be authenticated with the bot
+			requiresDirect: false, // Requires the message to refer to the bot
+			told: function() {
+				return {responseText: ["", ""]};
+			},
+			shutdown: function() {}
+		},
+		{
+			verbs: ["bye", "goodbye"],
 		}
-		onShutDown: function() {}
-	},
-	{
-		startUp: function() {},
-		verbs: ["bye", "goodbye"],
-		requiresAuth: false;
-
-		shutDown: function() {}
-	}]
+	]
 };
